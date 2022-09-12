dae4900d498f51099d9871c34e0792d458470c56
Folareen <sakawahab03@gmail.com>
2022-09-12 23:20:08 +0100

    routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Navigate, BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Loading from './components/Loading'
+import SignIn from './pages/SignIn'
+import Workspace from './pages/Workspace'
+import Channel from './pages/Channel'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase';
+import Header from './components/Header'
 
 function App() {
-  // const loggedIn = false
+  const [user, loading] = useAuthState(auth);
 
+  if(true){
+    return(
+      <Loading />
+    )
+  }
 
   return (
-    <div>
-      Hey Slack!
-    </div>
+    <Router>
+      {
+        !user?
+        <Routes>
+          <Route path='/' element={<Navigate replace to="/sign-in" />}/>
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+        :
+        <>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Workspace/>}/>
+          <Route path='/:channelId' element={<Channel/>} />
+          <Route path='/sign-in' element={<Navigate replace to="/" />}/>
+          <Route path='*' element={<Error />} />
+        </Routes>
+        </>
+      }
+    </Router>
   )
 }
 
