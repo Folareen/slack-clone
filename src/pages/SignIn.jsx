a03cbab2628aebef257c2b70141726cb4fd002b2
Folareen <sakawahab03@gmail.com>
2022-09-13 00:05:18 +0100

    sign in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { signInWithPopup} from "firebase/auth";
+import { auth } from '../../firebase';
+import { GoogleAuthProvider } from "firebase/auth";
+import {useNavigate } from 'react-router-dom';
+import { Box, Button, Center, Heading} from '@chakra-ui/react';
+import logo from '../assets/slack-new-logo.svg'
 
 const SignIn = () => {
+  const navigate = useNavigate()
+
+  const signIn = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth, provider).then(()=> {navigate('/')})
+  }
+
   return (
-    <div>SignIn</div>
+    <Center h={'100vh'} w={'100vw'}>
+      <Box >
+        <Heading as='h1' mb={5}>
+          Welcome to Slack!
+        </Heading>
+        <Box boxShadow={'xl'} p={4} border='1px' borderColor='gray.200' rounded='md' textAlign={'center'}>
+          <img src={logo} style={{width: '5rem', height: '5rem', margin: '2rem auto'}}/>
+          <Button onClick={signIn} colorScheme={'pink'} w={'full'} >
+            Sign In with Google
+          </Button>
+        </Box>
+      </Box>
+    </Center>
   )
 }
 
