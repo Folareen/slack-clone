95645adba615b334b6f505ce398050ab2f2a10c1
Folareen <sakawahab03@gmail.com>
2022-09-12 06:15:00 +0100

    firebase config

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBvWkJQMmZH7nV4xRmM64CajdIkYcMCJ8E",
   authDomain: "slack-clone-a385b.firebaseapp.com",
@@ -12,6 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "957160321859",
   appId: "1:957160321859:web:dc7cf7c4f7ea3d765535f6",
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
